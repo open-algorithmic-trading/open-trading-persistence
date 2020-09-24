55de26ebbba1f2a7d12b5b1062254067c0b7ca7f
awoods12 <awoods12@terpmail.umd.edu>
2020-09-23 23:28:09 -0400

    project init

---
+++ postgresql/src/main/java/com/opentrading/persistence/Application.java
@@ -0,0 +1,13 @@
+package com.opentrading.persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void helloWorld() {
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ postgresql/src/test/java/com/opentrading/persistence/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opentrading.opentradingpersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenTradingPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
